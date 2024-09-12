8bda7f8624c73b1bc5b449dd23dc393cea7cb668
Maikel-MK <maikelalejandro2008@gmail.com>
2024-09-11 20:40:16 -0400

    Delete portafolio ED directory

--- portafolio ED/practicas/icon/script.js
+++
@@ -1,7 +0,0 @@
-// Selecciona el elemento <link> del favicon
-const favicon = document.getElementById('favicon');
-
-// Cambia el favicon cuando el usuario intenta salir de la página
-window.addEventListener('beforeunload', function() {
-    favicon.hret='favicon-salida.ico.png'; // Cambia a tu favicon de salida
-});
