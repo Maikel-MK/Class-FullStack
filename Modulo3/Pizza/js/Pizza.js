0c1a0bff6ac4741744342d4b94a2842051de69aa
Maikel-MK <maikelalejandro2008@gmail.com>
2024-10-01 22:17:22 -0400

    "det"

--- Modulo3/Pizza/js/Pizza.js
+++ Modulo3/Pizza/js/Pizza.js
@@ -1,4 +1,7 @@
 const seleccion = document.querySelectorAll('.Extra')
+const pizzaPequeña = document.querySelector('#Pizzap')
+const pizzaMediana = document.querySelector('#Pizzam')
+const pizzaGrande = document.querySelector('#Pizzag')
 let suma = 0
 
 
@@ -29,18 +32,33 @@ const pizzas = document.querySelector('input[name="Pizza"]:checked')
 
 if(pizzas){
     suma += parseInt(pizzas.value)
+
+    // if(pizzaPequeña){
+    //     suma*1
+    //     console.log('llega1')
+    // }else if(pizzaMediana){
+    //     suma*2
+    //     console.log('llega2')
+    // }else if(pizzaGrande){
+    //     suma*4
+    //     console.log('llega3')
+    // }
 }
 
 let salsa = document.querySelectorAll('.Extra')
+
 salsa.forEach((Extras)=>{
     if(Extras.checked){
         suma += parseInt(Extras.value)
+        console.log('llega')
+
+
     }
    
 })
 
 document.getElementById('resultado').innerText= `Total de la Pizza: ${suma}`
-reset()
+//reset()
 }
 
 
