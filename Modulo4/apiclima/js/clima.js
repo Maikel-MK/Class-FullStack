f2eced35d77466fa4c5cb0a69cbcb2759bf07ce3
nbmnmjhh <jhjkhjhhh@gmail.com>
2024-10-02 15:09:45 -0400

    'tap'

--- Modulo4/apiclima/js/clima.js
+++ Modulo4/apiclima/js/clima.js
@@ -2,8 +2,7 @@
 
 //ahora hay que agregar los selectores crear los eventos:
 
-const ciudad = document.querySelector('#ciudad')
-const pais = document.querySelector('#pais')
+const contenedor = document.querySelector('.container')
 const  resultado = document.querySelector('#resultado')
 const formulario = document.querySelector('#formulario')
 
@@ -15,5 +14,32 @@ window.addEventListener('load',()=>{
 function buscarClima(e){
     e.preventDefault()
    // console.log('buscar clima')
-   console.log(ciudad,pais)
+   
+const ciudad = document.querySelector('#ciudad').value
+const pais = document.querySelector('#pais').value
+   //console.log(ciudad,pais)
+
+   if(ciudad === ""  || pais === ""){
+    mostrarError('Los campos son obligatorios')
+   }else{
+    console.log('campos llenos')
+   }
+}
+
+
+function mostrarError(mensaje){
+//hay un truco para evitar que el mensaje de error salga demasiado sin el setTimeout
+const alerta = document.querySelector('.bg-red-100')
+
+console.log(alerta)
+//si null undefine es un false
+
+if (!alerta){//aqui se niega la falsedad asi que se vuelve true "si no lo consigues CREALO"
+
+    const alertaM = document.createElement('div')
+        alertaM.innerHTML = mensaje
+        alertaM.classList.add('bg-red-100')
+
+        contenedor.appendChild(alertaM)
+}
 }
