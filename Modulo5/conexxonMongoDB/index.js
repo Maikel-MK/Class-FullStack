8b970e5300ffc086c765b027fa23349eb013bdfe
Maikel-MK <maikelalejandro2008@gmail.com>
2024-10-09 19:32:42 -0400

    "Mongo y mas"

---
+++ Modulo5/conexxonMongoDB/index.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const port = process.env.PORT || 9000
+
+app.listen(port, ()=> console.log('server listen on port', port))
+
+
+app.get
