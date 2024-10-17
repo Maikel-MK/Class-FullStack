38dd7c746462e5e5beff36b1fbf8dd81e0f78e39
Isabella Osechas <isabellaosechas@gmail.com>
2024-10-17 15:13:04 -0400

    'adt'

--- Modulo5/PROYECTO_Servidor/views/home/home.js
+++ Modulo5/PROYECTO_Servidor/views/home/home.js
@@ -2,6 +2,7 @@ const formC= document.querySelector('#form-create')
 const formL= document.querySelector('#form-login')
 const createInput = document.querySelector('#create-input')
 const notificacion = document.querySelector('.notification')
+const loginInput = document.querySelector('#login-input')
 
 formC.addEventListener('submit', async e=>{
     e.preventDefault()
@@ -9,8 +10,16 @@ formC.addEventListener('submit', async e=>{
     const respuesta = await fetch('http://localhost:3000/usuarios',{
         method:'GET'
     })
-    const users = respuesta.json()
-    console.log(users)
+    const users = await respuesta.json()
+    //console.log(users)
+
+    //validar
+    const user = users.find(i=>i.nombre===createInput.value)
+    //console.log(user)
+
+//foreach recorre pero no retorna en nada
+//find recorre y retorna en un booleano (true, false)
+
 
     if(!createInput.value){
 
@@ -23,8 +32,60 @@ formC.addEventListener('submit', async e=>{
         setTimeout(()=>{
             notificacion.classList.remove('show-notification')
         },2000)
+    }else if(user){
+        //si existe el usuario o ya esta registrado
+
+        notificacion.innerHTML = "El usuario ya existe"
+        notificacion.classList.add('show-notification')
+
+        setTimeout(()=>{
+            notificacion.classList.remove('show-notification')
+        },2000)
+    
+
     }else{
-        
+        //caso: no existe el usuario vamos a agregarlo
+        await fetch('http://localhost:3000/usuarios',{
+            method:'POST',
+            headers:{
+                'Content-Type':'application/json'//se pueden investigar mas tipos de content-Type
+            },
+            body:JSON.stringify({nombre:createInput.value})//recordar---
+        })
+
+        notificacion.innerHTML = `El usuario ${createInput.value} se ha creado Satisfactoriamente`
+        notificacion.classList.add('show-notification')
+
+        setTimeout(()=>{
+            notificacion.classList.remove('show-notification')
+        },2000)
+    
+        createInput.value = ""
+    }
+})
+
+formL.addEventListener('submit', async e=>{  //el async es el que debe estar con await
+    e.preventDefault()
+
+    const respuesta = await fetch('http://localhost:3000/usuarios',{
+        method:'GET'
+    })
+    const users = await respuesta.json()
+    //console.log(users)
+
+    //validar
+    const user = users.find(i=>i.nombre===loginInput.value)
+
+    if(!user){
+        notificacion.innerHTML = "El usuario no existe"
+        notificacion.classList.add('show-notification')
+
+        setTimeout(()=>{
+            notificacion.classList.remove('show-notification')
+        },2000)
+    
+    }else{
+        window.location.href = '/tareas'
     }
 
 })
